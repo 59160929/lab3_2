f45c746411b7460c903438314b5375056579a333
59160929 <42775843+59160929@users.noreply.github.com>
2018-09-07 23:38:25 +0700

    lab3_2 First commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import javax.swing.*;
+import java.io.FileReader;
 
 public class Main {
+
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        login loginPanel = new login();
-        frame.setContentPane(loginPanel.getMainPanel());
+        login  loginPanel  = new login();
+        frame.setContentPane(new login().getMainPanel());
         frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
--- src/login.java
+++ src/login.java
@@ -13,20 +13,20 @@ public class login {
         btnSubmit.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null,txtUserLogin.getText() + " " + new String(pwdUserPassword.getPassword()));
+            JOptionPane.showMessageDialog(null,txtUserLogin.getText()+" "+
+                    new String(pwdUserPassword.getPassword()));
             }
+
         });
         btnClear.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 txtUserLogin.setText("");
                 pwdUserPassword.setText("");
-
             }
         });
     }
-
-    public JPanel getMainPanel(){
-        return mainPanel;
+    public  JPanel getMainPanel(){
+        return  mainPanel;
     }
 }
